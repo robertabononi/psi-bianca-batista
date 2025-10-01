e7212117d3528d4a3604d8c80c3ac357335ffe6f
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2025-10-01 09:30:40 -0300

    feat: ajusta rota default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,8 @@ import { HomePage } from './pages/home-page/home-page';
 import { MenuPage } from './pages/menu-page/menu-page';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: HomePage,
-    },
-    {
-        path: 'menu',
-        component: MenuPage,
-    },
+    { path: '', component: HomePage },
+    { path: 'menu', component: MenuPage },
+    { path: '', redirectTo: '', pathMatch: 'full' },
+    { path: '**', redirectTo: '' }
 ];
